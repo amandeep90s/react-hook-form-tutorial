822d613d9aa7fd3fcdb8fba4773cbdb513facdc1
Amandeep Singh <programmeramandeep@gmail.com>
2024-09-13 19:40:59 +0530

    feat: Setup form

---
+++ src/components/YoutubeForm.tsx
@@ -0,0 +1,20 @@
+const YoutubeForm = () => {
+  return (
+    <div>
+      <form>
+        <label htmlFor='username'>Username</label>
+        <input type='text' id='username' name='username' />
+
+        <label htmlFor='email'>E-mail</label>
+        <input type='email' id='email' name='email' />
+
+        <label htmlFor='channel'>Channel</label>
+        <input type='text' id='channel' name='channel' />
+
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default YoutubeForm;
