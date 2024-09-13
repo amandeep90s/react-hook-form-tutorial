037610fe34fe69f53301acd7481a5cae63e9a4c3
Amandeep Singh <programmeramandeep@gmail.com>
2024-09-13 20:59:29 +0530

    feat: Default values

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -9,16 +9,10 @@ type FormValues = {
 
 const YoutubeForm = () => {
   const form = useForm<FormValues>({
-    defaultValues: async () => {
-      const response = await fetch(
-        'https://jsonplaceholder.typicode.com/users/1'
-      );
-      const data = await response.json();
-      return {
-        username: data.username,
-        email: data.email,
-        channel: '',
-      };
+    defaultValues: {
+      username: 'Batman',
+      email: '',
+      channel: '',
     },
   });
   const { register, control, handleSubmit, formState } = form;
